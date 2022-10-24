8bf5f1ed0794af610e47e38042af3c083bc3bc46
tylerj <tylerjcagle@gmail.com>
2022-10-24 16:49:00 -0600

    update test to handle promises

--- test/test.js
+++ test/test.js
@@ -81,7 +81,7 @@ describe("metainspector", function () {
     it("should have a title", function (done) {
       client = new MetaInspector("http://www.google.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.title.should.equal("Google");
         done();
       });
@@ -92,170 +92,144 @@ describe("metainspector", function () {
     it("should have keywords", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.keywords.should.be
           .instanceof(Array)
           .and.be.eql(["HTML", "CSS", "XML", "JavaScript"])
           .and.have.lengthOf(4);
         done();
       });
-
-      client.fetch();
     });
 
     it("keywords should be undefined if there is no keywords", function (done) {
       client = new MetaInspector("http://www.google.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.keywords.should.be.instanceof(Array).and.be.eql([]);
         done();
       });
-
-      client.fetch();
     });
 
     it("author should be undefined if there is no author", function (done) {
       client = new MetaInspector("http://www.google.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         should.not.exist(client.author);
 
         done();
       });
-
-      client.fetch();
     });
 
     it("charset should be undefined if there is no charset", function (done) {
       client = new MetaInspector("http://www.google.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         should.not.exist(client.charset);
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should have author", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.author.should.be.equal("Author Name");
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should have charset", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.charset.should.be.equal("UTF-8");
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should have links returned as an array", function (done) {
       client = new MetaInspector("http://www.google.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.links.length.should.equal(51);
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should have a description", function (done) {
       client = new MetaInspector("http://www.google.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.description.should.equal(
           "Search the world's information, including webpages, images, videos and more. Google has many special features to help you find exactly what you're looking for."
         );
         done();
       });
-
-      client.fetch();
     });
 
     it("should have a og:image with relative path and return as absolute", function (done) {
       client = new MetaInspector("http://www.fastandfurious7-film.com");
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.image.should.equal(
           "http://www.fastandfurious7-film.com/images/fb.jpg"
         );
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should have a og:description", function (done) {
       client = new MetaInspector("http://www.fastandfurious7-film.com");
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.ogDescription.should.equal(
           "Continuing the global exploits in the unstoppable franchise built on speed, Vin Diesel, Paul Walker and Dwayne Johnson lead the returning cast of Fast & Furious 7."
         );
         done();
       });
-
-      client.fetch();
     });
 
     it("should return undefined if the meta description is not defined when metaDescription used", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         should.not.exist(client.metaDescription);
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should find a secondary description if there is no description meta element", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.description.should.equal(
           "This is a new paragraph! This paragraph should be very long so we can grab it as the secondary description. What do you think of that?"
         );
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should find a the image based on the og:image tag if defined", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.image.should.equal("http://placehold.it/350x150");
 
         done();
       });
-
-      client.fetch();
     });
 
     it.skip("should return an array of absolute image paths for all images on the page", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.images.should.be
           .instanceof(Array)
           .and.be.eql([
@@ -269,30 +243,24 @@ describe("metainspector", function () {
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should return an array of rss or atom feeds if defined", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.feeds.length.should.equal(2);
         done();
       });
-
-      client.fetch();
     });
 
     it("should return the open graph title if defined", function (done) {
       client = new MetaInspector("http://www.simple.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         client.ogTitle.should.equal("I am an Open Graph title");
         done();
       });
-
-      client.fetch();
     });
 
     it("should emit errors", function (done) {
@@ -303,47 +271,39 @@ describe("metainspector", function () {
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should return the open graph type, if defined", function (done) {
       client = new MetaInspector("http://www.techsuplex.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         should.exist(client.ogType);
         client.ogType.should.equal("article");
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should return the last updated time, if defined", function (done) {
       client = new MetaInspector("http://www.techsuplex.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         should.exist(client.ogUpdatedTime);
         client.ogUpdatedTime.should.equal("2013-10-31T09:29:46+00:00");
 
         done();
       });
-
-      client.fetch();
     });
 
     it("should return the open graph locale, if defined", function (done) {
       client = new MetaInspector("http://www.techsuplex.com", {});
 
-      client.once("fetch", function () {
+      client.fetch().then(function () {
         should.exist(client.ogLocale);
         client.ogLocale.should.equal("en_US");
 
         done();
       });
-
-      client.fetch();
     });
   });
 });
